666fc0bc257f38016fdb9ea4f1a9cfc0e75ca6b6
BunkerBusterDev <bunkerbusterdev@gmail.com>
2022-02-23 22:35:08 +0900

    develop contentInstance upload and download

--- src/app.js
+++ src/app.js
@@ -4,14 +4,15 @@ import { useProtocol } from 'config';
 import CoreMqttConnector from 'core/mqttConnector';
 import CoreHttpConnector from 'core/httpConnector';
 
+global.initState = 'create-applicationEntity';
+
 let coreConnector = null;
 
 // Application Entity core
 if(useProtocol === 'mqtt') {
-    coreConnector = new CoreMqttConnector();
+    // WatchdogTimer.setWatchdogTimer('mqttConnector/initialize', 1, CoreMqttConnector.initialize);
 } else {
-    coreConnector = new CoreHttpConnector();
-    WatchdogTimer.setWatchdogTimer('httpConnector/initialize', 1, coreConnector.initialize);
+    WatchdogTimer.setWatchdogTimer('httpConnector/initialize', 1, CoreHttpConnector.initialize);
 }
 
 global.restart = async () => {
